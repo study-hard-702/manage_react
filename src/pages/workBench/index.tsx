1755f683f771b6bafa32987679508c460388a1e2
ZhangMengyuan-ZhouZhuan <zxd_workcode@126.com>
2019-07-08 14:35:46 +0800

    【工作台模块】

--- src/pages/workBench/index.tsx
+++ src/pages/workBench/index.tsx
@@ -7,6 +7,8 @@ import {actionCreators} from "../home/store";
 
 export interface WorkBenchProps {
   navTree?: any;
+  toDoList?: any;
+  shortcutList?: any;
   selectNode?: (props: any, id: string) => any;
 }
 
@@ -39,63 +41,50 @@ class WorkBench extends BaseComponent<WorkBenchProps, {}> {
   }
 
   doRender(): React.ReactElement<{}> {
+    const {
+      toDoList,
+      shortcutList,
+    } = this.props;
     return (
       <div className="WorkBench">
         <div className="WorkBench-toDo">
           <h4 className="WorkBench-toDo-title">待办事项</h4>
           <ul className="WorkBench-toDo-list">
-            <li className="toDo-item" onClick={() => this.goRoute('productCheck')}>
-              <div className="toDo-item-img">
-                <img src={require('../../statics/img/workBench/audit-will.png')} alt=''/>
-              </div>
-              <div className="toDo-item-text">
-                <p className="audit-text">待审核产品</p>
-                <p className="audit-num">67</p>
-              </div>
-            </li>
-            <li className="toDo-item" onClick={() => this.goRoute('productMy')}>
-              <div className="toDo-item-img">
-                <img src={require('../../statics/img/workBench/audit-yes.png')} alt=''/>
-              </div>
-              <div className="toDo-item-text">
-                <p className="audit-text">审核通过产品</p>
-                <p className="audit-num">23</p>
-              </div>
-            </li>
-            <li className="toDo-item" onClick={() => this.goRoute('productMy')}>
-              <div className="toDo-item-img">
-                <img src={require('../../statics/img/workBench/audit-no.png')} alt=''/>
-              </div>
-              <div className="toDo-item-text">
-                <p className="audit-text">审核未通过产品</p>
-                <p className="audit-num">43</p>
-              </div>
-            </li>
+            {
+              toDoList.map((item: any, index: number) => {
+                return (
+                  <li key={index}
+                      className="toDo-item"
+                      onClick={() => this.goRoute(item.path)}>
+                    <div className="toDo-item-img">
+                      <img src={item.imgUrl} alt=''/>
+                    </div>
+                    <div className="toDo-item-text">
+                      <p className="audit-text">{item.auditText}</p>
+                      <p className="audit-num">{item.auditNum}</p>
+                    </div>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
         <div className="WorkBench-shortcut">
           <h4 className="WorkBench-shortcut-title">快捷功能</h4>
           <ul className="WorkBench-shortcut-list">
-            <li className="shortcut-item" title="跳转到创建产品页面,进行新产品创建"
-                onClick={() => this.goRoute('productAdd')}>
-              <img src={require('../../statics/img/workBench/createProduct.png')} alt=''/>
-              <p className="shortcut-text">创建产品</p>
-            </li>
-            <li className="shortcut-item" title="跳转到我的产品查询页面"
-                onClick={() => this.goRoute('productMy')}>
-              <img src={require('../../statics/img/workBench/myProduct.png')} alt=''/>
-              <p className="shortcut-text">我的产品</p>
-            </li>
-            <li className="shortcut-item" title="跳转到所有产品查询页面，可对已有产品进行复用"
-                onClick={() => this.goRoute('productAll')}>
-              <img src={require('../../statics/img/workBench/allProduct.png')} alt=''/>
-              <p className="shortcut-text">产品复用</p>
-            </li>
-            <li className="shortcut-item" title="跳转到审核管理页面，对待审核产品进行审核"
-                onClick={() => this.goRoute('productCheck')}>
-              <img src={require('../../statics/img/workBench/auditProduct.png')} alt=''/>
-              <p className="shortcut-text">审核管理</p>
-            </li>
+            {
+              shortcutList.map((item: any, index: number) => {
+                return (
+                  <li key={index}
+                      className="shortcut-item"
+                      title={item.title}
+                      onClick={() => this.goRoute(item.path)}>
+                    <img src={item.imgUrl} alt=''/>
+                    <p className="shortcut-text">{item.shortcutText}</p>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
       </div>
@@ -106,6 +95,52 @@ class WorkBench extends BaseComponent<WorkBenchProps, {}> {
 function mapStateToProps(state: any): WorkBenchProps {
   return {
     navTree: state.getIn(['home', 'navTree']),
+    toDoList: [
+      {
+        path: 'productCheck',
+        imgUrl: require('../../statics/img/workBench/audit-will.png'),
+        auditText: '待审核产品',
+        auditNum: 67,
+      },
+      {
+        path: 'productMy',
+        imgUrl: require('../../statics/img/workBench/audit-yes.png'),
+        auditText: '审核通过产品',
+        auditNum: 23,
+      },
+      {
+        path: 'productMy',
+        imgUrl: require('../../statics/img/workBench/audit-no.png'),
+        auditText: '审核未通过产品',
+        auditNum: 43,
+      },
+    ],
+    shortcutList: [
+      {
+        path: 'productAdd',
+        imgUrl: require('../../statics/img/workBench/createProduct.png'),
+        shortcutText: '创建产品',
+        title: '跳转到创建产品页面,进行新产品创建',
+      },
+      {
+        path: 'productMy',
+        imgUrl: require('../../statics/img/workBench/myProduct.png'),
+        shortcutText: '我的产品',
+        title: '跳转到我的产品查询页面',
+      },
+      {
+        path: 'productAll',
+        imgUrl: require('../../statics/img/workBench/allProduct.png'),
+        shortcutText: '产品复用',
+        title: '跳转到所有产品查询页面，可对已有产品进行复用',
+      },
+      {
+        path: 'productCheck',
+        imgUrl: require('../../statics/img/workBench/auditProduct.png'),
+        shortcutText: '审核管理',
+        title: '跳转到审核管理页面，对待审核产品进行审核',
+      },
+    ]
   };
 }
 
